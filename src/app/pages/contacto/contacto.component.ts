564ad5237add486287ad086ce52fb33984551e57
AliciaNazar <alicianazarab@gmail.com>
2024-02-29 18:37:06 -0300

    contacto listo, faltan estilos

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,NgClass],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit{
+  
+  formularioContacto!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.formularioContacto = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]],
+    })
+  }
+
+  enviar(event: Event){
+    event.preventDefault();
+    console.log(this.formularioContacto.value);
+  }
+  ngOnInit(): void {
+    
+  }
+
+  hasErrors(field: string, typeError: string){
+    return this.formularioContacto.get(field)?.hasError(typeError)&& this.formularioContacto.get(field)?.touched;
+  }
 }
